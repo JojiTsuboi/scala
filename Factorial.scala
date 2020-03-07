02bb1f8fe0716a3dd7d535446ba0960f1c634c9b
JojiTsuboi <joji0903@gmail.com>
2020-03-07 14:48:34 +0900

    first

---
+++ Factorial.scala
@@ -0,0 +1,8 @@
+import scala.math.BigInt
+
+object Factorial extends App {
+  def factorial(i: BigInt, acc: BigInt): BigInt =
+    if (i == 0) acc else factorial(i - 1, i * acc)
+
+  println(factorial(100000, 1))
+}
---
+++ SimpleSearch.scala
@@ -0,0 +1,7 @@
+object SimpleSearch extends App {
+  val text = "カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq
+  val pattern = "ドワンゴ".toSeq
+  val matchIndex = Seq()
+
+  println(s"出現場所: ${matchIndex}")
+}
